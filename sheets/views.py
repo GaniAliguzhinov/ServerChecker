825a6c75b2e35ccc6449b4fe2d013660457d29b2
Gani Aliguzhinov <aliguzhinov.gani@gmail.com>
2020-12-13 19:41:02 +0600

    added redirect from /sheets/ to urls.py of sheets app

--- sheets/views.py
+++ sheets/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import UploadSheetForm
+import xlrd
 
-# Create your views here.
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadSheetForm(request.POST, request.FILES)
+        if form.is_valid():
+            instance = UploadSheetForm(file=request.FILES['file'])
+            instance.save()
+            return HttpResponseRedirect(reverse('sheets.views.list'))
+    else:
+        form = UploadSheetForm()
+    return render(request, 'upload.html', {'form': form})
